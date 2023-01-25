6292529a4a1047de25d5ce0dbdf3dee2cfb2f5f1
IMakeBotsForYou <40301997+IMakeBotsForYou@users.noreply.github.com>
2023-01-25 14:48:34 +0200

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,78 +1,111 @@
 // Importing modules
 import React, { useState, useEffect } from "react";
 import "./App.css";
-  
-function App() {
-    // usestate for setting a javascript
-    // object for storing and using data
-    const [data, setdata] = useState({
+import ReactDOM from "react-dom/client";
+
+let global_data = {}
+
+
+function MyForm() {
+  const [inputs, setInputs] = useState({});
+  const [data, setData] = useState({
         time: 0,
         distance: 0,
-        warning: "None",
+        warning: "None"
     });
-  
-    // Using useEffect for single rendering
-    useEffect(() => {
-        // Using fetch to fetch the api from 
-        // flask server it will be redirected to proxy
-        fetch("/", {
-            "mode": "POST",
-            "body": JSON.stringify()
-        }).then((res) =>
-            res.json().then((data) => {
-                // Setting a data from api
-                setdata({
-                    time: data.time,
-                    distance: data.distance,
-                    warning: data.warning,
-                });
-            })
-        );
-    }, []);
-  
-    return (
-        <div className="App">
-            <header>
-                <h1>Takeoff calculator</h1>
-            </header>
-        </div>
-    );
-}
-  
-export default App;
 
-
-
-/*class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(inputs => ({...inputs, [name]: value}))
+    console.log(inputs);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+  const handleSubmit = (event) => {
     event.preventDefault();
+    // Using fetch to fetch the api from 
+    // flask server it will be redirected to proxy
+    if (!inputs)
+        inputs = {}
+    console.log(inputs);
+    fetch("/", {
+        "method": "POST",
+        "body": JSON.stringify(inputs),
+        "headers": {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          }
+    }).then((res) =>
+        res.json().then((data) => {
+            // Setting a data from api
+            setData(data)
+        })
+    ).catch(error => console.log(error));
   }
 
-  render() {
+  return ( 
+    <>
+    <form onSubmit={handleSubmit}>
+      <label>Load:  </label>
+      <input 
+        type="number" 
+        name="load" 
+        value={inputs.load || 0} 
+        onChange={handleChange}
+      /><br></br>
+      <label>Enter your Base Mass:  </label>
+        <input 
+          type="number" 
+          name="base_mass" 
+          value={inputs.base_mass || 35000} 
+          onChange={handleChange}
+        /><br></br>
+      <label>Enter your Force:  </label>
+        <input 
+          type="number" 
+          name="force" 
+          value={inputs.force || 100000} 
+          onChange={handleChange}
+        /><br></br>
+      <label>Enter your Required Speed:  </label>
+        <input 
+          type="number" 
+          name="req_speed" 
+          value={inputs.req_speed || 140} 
+          onChange={handleChange}
+        /><br></br>
+        <input type="submit" />
+    </form>
+    <div>
+        <p>
+          {data.time}s
+        </p>
+        <p>
+          {data.distance}m
+        </p>
+        <br></br>
+        <p>
+          Warning: {data.warning}
+        </p>
+    </div>
+    </>
+  )
+}
+//
+
+function App() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+        <>
+          <div className="App">
+            <header>
+              <h1>Takeoff calculator</h1>
+            </header>
+          </div>
+          <div style={{marginLeft: 50 + 'px'}}>
+          <MyForm/>
+          </div>
+        </>
+    ); //
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<NameForm />);*/
+export default App;
